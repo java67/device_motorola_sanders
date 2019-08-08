31ad88eea6fc25a35b74fe0b48607d89dbe27c32
marshmello61 <ultramayur123@gmail.com>
2019-08-08 19:00:00 +0530

    Sanders: spectrum: remove spectrum support
    * basically a revert of the commit.
    
    This reverts commit baf36a1cdfe9f66abe10163811d282158ddc9b06.
    
    Signed-off-by: Mayur (marshmello61/marshmello_61) <ultramayur123@gmail.com>
    
    Signed-off-by: marshmello61 <ultramayur123@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -91,6 +91,7 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.sanders.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
+<<<<<<< HEAD
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.spectrum.rc
@@ -115,3 +116,5 @@ LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.safailnet.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
+=======
+>>>>>>> e377680... Sanders: spectrum: remove spectrum support
