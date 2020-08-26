3f43c4158371576b26c1f899ce0e9598485de1da
V1NAY007 <shandilyavinay0@gmail.com>
2020-08-26 05:05:47 +0000

    bringup for p404

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-	$(LOCAL_DIR)/rr_sanders.mk
+	$(LOCAL_DIR)/p404_sanders.mk
 
 COMMON_LUNCH_CHOICES := \
-	rr_sanders-userdebug
+	p404_sanders-userdebug
