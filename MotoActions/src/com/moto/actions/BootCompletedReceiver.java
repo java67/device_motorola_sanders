3f5f8eaff39e8b76b4187aad88aa1578d8d2a7b9
V1NAY007 <shandilyavinay0@gmail.com>
2020-10-01 12:59:44 +0000

    Revert "add motoaction from saisamy dt"
    This reverts commit eb00451f0f3c17cbf1029ba7e451626bd43906de.

--- MotoActions/src/com/moto/actions/BootCompletedReceiver.java
+++ MotoActions/src/com/moto/actions/BootCompletedReceiver.java
@@ -27,9 +27,6 @@ import android.content.ServiceConnection;
 import android.os.IBinder;
 import android.util.Log;
 
-import com.moto.actions.dirac.DiracUtils;
-import com.moto.actions.DisplayCalibration;
-import com.moto.actions.util.FileUtils;
 import com.moto.actions.actions.Constants;
 import com.moto.actions.ServiceWrapper.LocalBinder;
 
@@ -53,8 +50,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         }
 
         context.startService(new Intent(context, ServiceWrapper.class));
-        new DiracUtils(context).onBootCompleted();
-            DisplayCalibration.restore(context);
     }
 
     private ServiceConnection serviceConnection = new ServiceConnection() {
